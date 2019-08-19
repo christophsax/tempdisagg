ebe38cc8bbda3e9fddb67318f83fe1bdcc64f55a
Christoph <christoph@cynkra.com>
2019-08-19 09:23:52 +0200

    rox upd

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -6,7 +6,7 @@ library(tsbox)
 
 test_that("to daily works", {
   m <- td(ts_df(mdeaths) ~ 1, to = "day", method = "fast")
-  expect_is(predict(m), "tbl_df")
+  expect_is(predict(m), "data.frame")
 })
 
 
